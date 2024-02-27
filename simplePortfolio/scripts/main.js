d179a1779a906ee42d074a065d518c8600dce363
RealShivomSharma <shars119@mcmaster.ca>
2024-02-26 19:56:17 -0600

    Added some simple styling for the website, it is making some progress

---
+++ simplePortfolio/scripts/main.js
@@ -0,0 +1,16 @@
+
+/*
+const nav = document.getElementById('nav');
+
+function handleScroll() {
+	if (window.scrollY > 100) {
+		nav.classList.add('hidden');
+	} 
+	else {
+		nav.classList.remove('hidden');
+	}
+}
+
+window.addEventListener('scroll', handleScroll);
+*/
+
